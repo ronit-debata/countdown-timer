a2e119f73846a06bde6539d8c876e0b7a83eb414
[ronit-debata] <[ronit.debata90@gmail.com]>
2021-09-23 20:24:55 +0530

    new year countdown

---
+++ script.js
@@ -0,0 +1,16 @@
+var secs = 10;
+
+setTimeout(Decrement,1000); 
+
+function Decrement() {
+    document.getElementById("timer").innerHTML = secs;
+    secs--;
+    if(secs !== -1)
+    {
+        setTimeout(Decrement,1000);
+    }
+    else{
+        document.getElementById("timer").classList.add("message");
+        document.getElementById("timer").innerHTML = "Happy New Year!";
+    }
+}
